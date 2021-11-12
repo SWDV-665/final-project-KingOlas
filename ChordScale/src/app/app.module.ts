844c506faf4d2ed2b79981bd3670a0979a1a422a
KingOlas <olaslovesbass@gmail.com>
2021-11-11 21:04:59 -0500

    Debugging

--- ChordScale/src/app/app.module.ts
+++ ChordScale/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { Howl, Howler } from 'howler';
+import { Howl } from 'howler';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
